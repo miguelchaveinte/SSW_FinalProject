6731e6dbd0b5e922672169d9a781f703e6b39158
angel <angel@LAPTOP-KGH479RG>
2022-04-14 11:47:17 +0200

    Creada clase Curso

---
+++ practica_ssw2/src/main/java/ulearn/model/Curso.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulearn.model;
+
+import java.io.Serializable;
+import javax.servlet.http.Part;
+
+/**
+ * Clase que implementa un curso.
+ * @author angel
+ */
+public class Curso implements Serializable{
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+    private Part imagen;
+    private float duracion;
+    private String categoria;
+    private int creador;
+    
+    /**
+     * Constructor sin parametros.
+     */
+    public Curso(){
+    id=0;
+    nombre="";
+    descripcion="";
+    precio=0;
+    imagen=null;
+    duracion=0;
+    categoria="";
+    creador=0;
+    }
+    
+    /**
+     * Constructor con parametros.
+     */
+    public Curso(int id, String nombre, String descripcion, float precio, Part imagen, float duracion, String cateogria, int creador){
+    this.id=id;
+    this.nombre=nombre;
+    this.descripcion=descripcion;
+    this.precio=precio;
+    this.imagen=imagen;
+    this.duracion=duracion;
+    this.categoria=categoria;
+    this.creador=creador;
+    }
+
+    //Getters
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public Part getImagen() {
+        return imagen;
+    }
+
+    public float getDuracion() {
+        return duracion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getCreador() {
+        return creador;
+    }
+    
+    //Setters
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public void setImagen(Part imagen) {
+        this.imagen = imagen;
+    }
+
+    public void setDuracion(float duracion) {
+        this.duracion = duracion;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setCreador(int creador) {
+        this.creador = creador;
+    }
+    
+    
+}
+
+
+
+
